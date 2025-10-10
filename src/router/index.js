fc42e204798b986cc7b0a2f37bcd65dc8123d975
Zyzero1 <mazza050901@gmail.com>
2025-10-10 17:47:14 +0700

    Refactor router configuration to remove unused About route and update paths for list-survey and data-responden components

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import About from '../components/About.vue'
 import Esurvei from '../components/Esurvei.vue'
 import KategoriOPD from '../components/kategori-opd.vue'
 import KategoriDinas from '../components/kategori-dinas.vue'
+import ListSurvey from '../components/list-survey.vue' 
 import DataResponden from '../components/data-responden.vue'
 import Survei from '../components/survei.vue'
 import KritikSaran from '../components/kritik-saran.vue'
@@ -12,9 +12,9 @@ const routes = [
   { path: '/', component: Esurvei },
   { path: '/kategori-opd', component: KategoriOPD },
   { path: '/kategori-dinas/:id?', component: KategoriDinas },
-  { path: '/about', component: About },
-  { path: '/data-responden', component: DataResponden },
-  { path: '/survei', component: Survei },
+  { path: '/list-survey/:id', component: ListSurvey },
+  { path: '/data-responden/:id', component: DataResponden },
+  { path: '/survei/:id', component: Survei },
   { path: '/kritik-saran', component: KritikSaran },
   { path: '/finish', component: Finish },
   { path: '/:pathMatch(.*)*', redirect: '/' }
